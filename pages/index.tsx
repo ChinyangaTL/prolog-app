c415cc05824f0012911b602b311b70ec8718fc37
Les Chinyanga <les.chinyanga@africacode.academy>
2023-04-28 11:35:54 +0200

    feat: fetch cms data for hero

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { Routes } from "@config/routes";
 
@@ -71,26 +71,61 @@ const ModalCard = styled.div`
   transform: translate(-50%, -50%);
 `;
 
-const modalOpenStyles = {
-  position: "fixed",
-  top: 0,
-  left: 0,
-  zIndex: 1040,
-  width: "100vw",
-  height: "100vh",
-  backgroundColor: "rgba(0, 0, 0, 0.4)",
-  backdropFilter: "blur(15px)",
+type CMSSection = {
+  image: { src: string; width: number; height: number };
+  sectionType: string;
+  subtitle: string;
+  theme: string;
+  title: string;
+};
+
+type CMSData = {
+  meta: {
+    description: string;
+    image: string;
+    title: string;
+  };
+  sections: CMSSection[];
 };
 
 const IssuesPage = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const handleOpenModal = () => {
-    setIsModalOpen(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const [heroSection, setHeroSection] = useState<CMSSection | null>(null);
+
+  const modalOpenStyles = {
+    position: "fixed",
+    top: 0,
+    left: 0,
+    zIndex: 1040,
+    width: "100vw",
+    height: "100vh",
+    backgroundColor: isModalOpen ? "rgba(0, 0, 0, 0.4)" : "transparent",
+    backdropFilter: isModalOpen ? "blur(15px)" : "none",
   };
+
+  const getHeroSection = (sections: CMSSection[]) =>
+    sections.find((section) => section.sectionType === "hero");
+
+  const fetchCMSData = async () => {
+    setIsLoading(true);
+    const res = await fetch("https://prolog-api.profy.dev/content-page/home");
+    const data: CMSData = await res.json();
+    console.log(data);
+    const { sections } = data;
+
+    const heroSection = getHeroSection(sections);
+    heroSection && setHeroSection(heroSection);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchCMSData();
+  }, []);
 
   return (
     <div
-      style={isModalOpen ? modalOpenStyles : { height: "100vh" }}
+      style={modalOpenStyles}
       onClick={() => isModalOpen && setIsModalOpen(false)}
     >
       <Header>
@@ -121,6 +156,43 @@ const IssuesPage = () => {
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img src="/icons/message.svg" alt="Contact" />
       </ContactButton>
+      <main
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+          gap: "2rem",
+          padding: "2rem",
+        }}
+      >
+        {isLoading ? (
+          <h1>Loading...</h1>
+        ) : (
+          heroSection && (
+            <section
+              style={{
+                background: `url(${heroSection.image.src})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                height: "100vh",
+              }}
+            >
+              <div>
+                <h1>{heroSection.title}</h1>
+                <p>{heroSection.subtitle}</p>
+              </div>
+              <img
+                src={heroSection.image.src}
+                alt="Hero"
+                width={heroSection.image.width}
+                height={heroSection.image.height}
+              />
+            </section>
+          )
+        )}
+      </main>
+
       {isModalOpen && (
         <ModalCard>
           <h1>Modal</h1>
